99ab04edbad9a65b074f37f6415b7aefdf0495bb
MR.Dev <marcorettoremattes@gmail.com>
2024-02-27 20:16:35 -0300

    🐛 Now we set currectly the timezone

--- src/features/weather/weather.service.ts
+++ src/features/weather/weather.service.ts
@@ -13,7 +13,7 @@ export const getWeather = async ({
   const { data } = await axios<TForecast>({
     url: `${
       import.meta.env.VITE_API_FORECAST
-    }/forecast?latitude=${lat}&longitude=${lon}&forecast_days=16&daily=uv_index_max,apparent_temperature_max,apparent_temperature_min,precipitation_probability_mean,weathercode,windspeed_10m_max&current_weather=true&current=is_day`,
+    }/forecast?latitude=${lat}&longitude=${lon}&forecast_days=16&daily=uv_index_max,apparent_temperature_max,apparent_temperature_min,precipitation_probability_mean,weathercode,windspeed_10m_max&current_weather=true&current=is_day&timezone=auto`,
     method: "get",
   });
 
